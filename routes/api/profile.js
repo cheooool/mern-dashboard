8fa94ea4360e121df68729ef77f139fd278f3980
g.cheooool <g.cheooool@gmail.com>
2019-05-31 03:39:44 +0900

    Profile API 추가
    - GET /api/profile/me
      현재 사용자 프로필
    - POST /api/profile
       프로필 생성 및 업데이트
    - GET /api/profile
      전체 사용자 프로필

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,173 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    let profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+    if (!profile) {
+      return res.status(400).json({
+        msg: '이 사용자는 프로필이 등록되지 않았습니다.'
+      });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route    POST api/profile
+// @desc     Create or update user profile
+// @access   Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', '직업을 입력해주세요.')
+        .not()
+        .isEmpty(),
+      check('skills', '기술을 입력해주세요.')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin
+    } = req.body;
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) {
+      profileFields.company = company;
+    }
+    if (website) {
+      profileFields.website = website;
+    }
+    if (location) {
+      profileFields.location = location;
+    }
+    if (bio) {
+      profileFields.bio = bio;
+    }
+    if (status) {
+      profileFields.status = status;
+    }
+    if (githubusername) {
+      profileFields.githubusername = githubusername;
+    }
+    if (skills) {
+      profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+
+    profileFields.social = {};
+    if (youtube) {
+      profileFields.social.youtube = youtube;
+    }
+    if (twitter) {
+      profileFields.social.twitter = twitter;
+    }
+    if (facebook) {
+      profileFields.social.facebook = facebook;
+    }
+    if (linkedin) {
+      profileFields.social.linkedin = linkedin;
+    }
+    if (instagram) {
+      profileFields.social.instagram = instagram;
+    }
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        // update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      // Create
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route    GET api/profile
-// @desc     Test route
+// @desc     Get all profiles
 // @access   Public
-router.get('/', (req, res) => res.send('Profile route'));
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.mesage);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({
+        msg: '프로필을 찾을 수 없습니다.'
+      });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.mesage);
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({
+        msg: '프로필을 찾을 수 없습니다.'
+      });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
