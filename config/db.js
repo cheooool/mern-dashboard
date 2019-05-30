0d053ce0416d0ea347564d5baab1be76bcd37a12
g.cheooool <g.cheooool@gmail.com>
2019-05-30 16:31:51 +0900

    mongoose를 이용해 mongodb 연결

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+
+    // 실패로 인해 프로세스 종료
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
