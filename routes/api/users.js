8097364e9334418534af5cac77520bc34acf29f7
g.cheooool <g.cheooool@gmail.com>
2019-05-30 17:06:09 +0900

    Register User validation 체크 로직 추가

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route    GET api/users
-// @desc     Test route
+// @route    POST api/users
+// @desc     유저 생성
 // @access   Public
-router.get('/', (req, res) => res.send('Users route'));
+router.post(
+  '/',
+  [
+    check('name', '이름을 입력해주세요.')
+      .not()
+      .isEmpty(),
+    check('email', '올바른 이메일을 입력해주세요.').isEmail(),
+    check('password', '비밀번호를 6자이상 입력해주세요.').isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('Users route');
+  }
+);
 
 module.exports = router;
