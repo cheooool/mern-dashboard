8acd2961f236a7ba84883185503cc610721fb744
g.cheooool <g.cheooool@gmail.com>
2019-05-30 18:27:04 +0900

    auth get, post 추가
    - get 유저 정보 반환
    - post 로그인

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,89 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
 
 // @route    GET api/auth
 // @desc     Test route
 // @access   Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    POST api/auth
+// @desc     사용자 인증과 토큰 반환
+// @access   Public
+router.post(
+  '/',
+  [
+    check('email', '올바른 이메일을 입력해주세요.').isEmail(),
+    check('password', '비밀번호를 입력해주세요.').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // 유저가 존재할 경우
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: [{ msg: '사용자 정보가 잘못되었습니다.' }] });
+      }
+
+      // 비밀번호 체크
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ error: [{ msg: '사용자 정보가 잘못되었습니다.' }] });
+      }
+
+      // jwt 반환
+      const payload = {
+        user: {
+          id: user._id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            token
+          });
+        }
+      );
+
+      // res.send('Users Register');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
