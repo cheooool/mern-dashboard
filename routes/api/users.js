ce944054ce86da068ba56534921b71ad3f4f5a24
g.cheooool <g.cheooool@gmail.com>
2019-05-30 17:42:29 +0900

    user 생성 로직 추가
    - gravatar 연결
    - bcrypt 연결
    - mongodb save 추가

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
 
 // @route    POST api/users
 // @desc     유저 생성
@@ -14,12 +18,52 @@ router.post(
     check('email', '올바른 이메일을 입력해주세요.').isEmail(),
     check('password', '비밀번호를 6자이상 입력해주세요.').isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('Users route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      // 유저가 존재할 경우
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: [{ msg: '이미 존재하는 사용자입니다.' }] });
+      }
+
+      // 이메일 기반으로 gravatar 가져오기
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      // 비밀번호 암호화
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // jwt 반환
+
+      res.send('Users Register');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
