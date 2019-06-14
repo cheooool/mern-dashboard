83080872cf1eda3d581b038d40d1471298831f4a
g.cheooool <g.cheooool@gmail.com>
2019-06-14 16:35:11 +0900

    현재 로그인한 계정 프로필 가져오기 추가

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
+import Spinner from '../layout/Spinner';
 
-const Dashboard = props => {
-  return <div>Dashboard</div>;
+const Dashboard = ({
+  auth: { user },
+  profile: { profile, loading },
+  getCurrentProfile
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user"> Welcome {user && user.name}</i>
+      </p>
+      {profile !== null ? (
+        <Fragment>has</Fragment>
+      ) : (
+        <Fragment>
+          <p>프로필을 설정하지 않았습니다. 프로필을 추가해주세요.</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            프로필 생성
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
 
-export default Dashboard;
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile }
+)(Dashboard);
